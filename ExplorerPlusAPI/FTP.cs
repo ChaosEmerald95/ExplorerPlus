c468372dd442f586740090d65964a0444562facf
ChaosEmerald95 <p.loessner95@gmail.com>
2016-02-11 14:09:21 +0100

    FTP-Function for getting Filelist for url implemented

---
+++ ExplorerPlusAPI/FTP.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.IO;
+
+namespace ExplorerPlus.API
+{
+    public static class FTP
+    {
+        public static List<string> GetFilesInDirectory(string url)
+        {
+            var request = (FtpWebRequest)WebRequest.Create(url);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            List<string> l = new List<string>();
+            using (var response = (FtpWebResponse)request.GetResponse())
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    var reader = new StreamReader(responseStream);
+                    while (!reader.EndOfStream)
+                    {
+                        l.Add(reader.ReadLine());                        
+                    }
+                }
+            }
+            return l;
+        }
+
+        public static List<string> GetFilesInDirectory(string url, string username, string password)
+        {
+            var request = (FtpWebRequest)WebRequest.Create(url);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            request.Credentials = new NetworkCredential(username, password);
+            List<string> l = new List<string>();
+            using (var response = (FtpWebResponse)request.GetResponse())
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    var reader = new StreamReader(responseStream);
+                    while (!reader.EndOfStream)
+                    {
+                        l.Add(reader.ReadLine());
+                    }
+                }
+            }
+            return l;
+        }
+    }
+}
